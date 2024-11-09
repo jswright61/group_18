55b1f1afd45d2af8254079a9ebc4a3a3c38dc454
jswright61 <scott@wrightzone.com>
2024-11-09 11:24:33 -0500

    Upgraded Mute Functionality in SoundPlayer
    Now track volume level in the class rather than letting Pygame track it.
    Allows volume level restored after a mute (previously mute set volume to zero)
    Added optional parameter to constructor, start_muted, so the game can start with the loop music muted.

--- sound_player.py
+++ sound_player.py
@@ -1,18 +1,21 @@
 import pygame as pg
 
 class SoundPlayer:
-  def __init__(self, initial_volume = 0.6):
+  def __init__(self, volume = 0.4, start_muted = False):
     pg.mixer.init()
     self.click = pg.mixer.Sound("click.wav")
     self.restart = pg.mixer.Sound("restart.wav")
     self.high_score = pg.mixer.Sound("high_score.wav")
     self.win = pg.mixer.Sound("win.wav")
     self.time_expired = pg.mixer.Sound("timeout.wav")
+    self.vol_level = volume
+    self.muted = start_muted
     pg.mixer.music.load("background_music.wav")
-    pg.mixer.music.set_volume(initial_volume)  # Adjust volume
+    pg.mixer.music.set_volume(self.vol_level)
 
   def bg_start(self):
-    pg.mixer.music.play(-1)
+    if not self.muted:
+      pg.mixer.music.play(-1)
 
   def bg_stop(self):
     pg.mixer.music.stop()
@@ -33,16 +36,26 @@ class SoundPlayer:
         print(f"{sound_name} is not a valid sound")
 
   def mute(self):
-    pg.mixer.music.set_volume(0)
+    if self.muted:
+      if self.vol_level < 0.1:
+        self.vol_level = 0.1
+        pg.mixer.music.set_volume(self.vol_level)
+      pg.mixer.music.play(-1)
+    else:
+      pg.mixer.music.stop()
+    self.muted = not self.muted
 
   def volume_up(self):
-    volume = pg.mixer.music.get_volume()
-    if volume < 1.0:
-      volume += 0.1
-      pg.mixer.music.set_volume(min([1.0, volume]))
+    if self.muted:
+      # just unmute if volume was muted and up volume is pressed
+      self.mute()
+      return
+    self.vol_level = min([1.0, self.vol_level + 0.1])
+    pg.mixer.music.set_volume(self.vol_level)
 
   def volume_down(self):
-    volume = pg.mixer.music.get_volume()
-    if volume > 0.0:
-      volume -= 0.1
-      pg.mixer.music.set_volume(max([0.0, volume]))
+    if self.muted:
+      # we're not going to adjust volume when sound is muted
+      return
+    self.vol_level = max([0.0, self.vol_level - 0.1])
+    pg.mixer.music.set_volume(self.vol_level)
